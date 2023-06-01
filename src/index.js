e22ccb98249469169eb2d3b67d50849f2b1befa4
3600-abhi <abhishekjaiswal3600@gmail.com>
2023-06-01 11:26:21 +0530

    Nodejs Project Template

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const { ServerConfig, Logger } = require("./config");
+const apiRoutes = require("./routes");
+
+const app = express();
+
+app.use("/api", apiRoutes);
+
+app.listen(ServerConfig.PORT, () => {
+  console.log(`successfully started the server at PORT : ${ServerConfig.PORT}`);
+  Logger.info("successfully started the server", "root", {});
+});
